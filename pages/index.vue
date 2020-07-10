fce800ca652a2890c4ad5461d567a7c166efb27c
Raphaelle <raph0713@gmail.com>
2020-07-09 21:13:07 -0400

    init

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <header-section></header-section>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+    <div>lorem</div>
+
+  </div>
+</template>
+
+<script>
+import headerSection from '@/components/headerSection.vue'
+export default {
+  components: { headerSection }
+}
+</script>
